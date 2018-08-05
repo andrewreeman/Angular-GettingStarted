6d614a63c9c205f038b41dcf1f58967726fd9295
areeman <areeman@controlpoint.co.uk>
2018-08-05 21:43:16 +0100

    latest version

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core'
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <div>  
+    <h1>{{title}}</h1>
+    <pm-products></pm-products>    
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title: string = 'App Component'
 }
+
+
+
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'pm-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'Angular: Getting Started';
+// }
