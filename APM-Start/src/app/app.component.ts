d508dfce1b45c1adc9959e5dfa2c65d59d6d9c7c
areeman <areeman@controlpoint.co.uk>
2018-08-17 01:13:37 +0100

    init tutorial commit

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import {Component} from '@angular/core'
 
 @Component({
-  selector: 'pm-root',
-  template: `
-  <div>  
-    <h1>{{title}}</h1>
-    <pm-products></pm-products>    
-    </div>
-  `
+  selector: 'pm-root',  
+  styleUrls: ['./app.component.scss'],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title: string = 'App Component'
+  pageTitle: string = 'App Component'
 }
 
 
